f3bf010971b9134e829325e807482b159103f9ff
Zakirov Oleksandr <firstname.lastname@laurea.fi>
2019-09-26 17:04:37 +0300

    Component for meteo added.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { MeteoComponent } from './meteo/meteo.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, MeteoComponent]
 })
 export class HomePageModule {}
