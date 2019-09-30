bfc5c55e953f547708dceb102501bab7ce11f42e
Lucas <lucas.cheminade@epitech.eu>
2019-09-30 16:08:21 +0300

    FEAT(MeteoService): push fix with the git tree

---
+++ src/app/models/meteo.ts
@@ -0,0 +1,14 @@
+export class Meteo {
+    city: string;
+    temperature: number;
+    humidity: number;
+    windSpeed: number;
+
+    deserialize(input: any): this {
+        this.city = input.name;
+        this.temperature = input.main.temp;
+        this.humidity = input.main.humidity;
+        this.windSpeed = input.wind.speed;
+        return this;
+    }
+}
--- src/app/services/meteo.service.ts
+++ src/app/services/meteo.service.ts
@@ -1,11 +1,26 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Meteo } from '../models/meteo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeteoService {
 
-private readonly apiKey = '3481483743ad645cbdb84b425581d2f1';
+private readonly apiKey: string  = '3481483743ad645cbdb84b425581d2f1';
+private readonly baseUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getMeteo(city: string, countryCode: string, units: string = 'metric'): Observable<Meteo> {
+    let params = new HttpParams();
+
+    params = params.append('q', city + ',' + countryCode);
+    params = params.append('units', units);
+    params = params.append('appid', this.apiKey);
+    return this.httpClient
+    .get(this.baseUrl, {params: params}).pipe(map(response => new Meteo().deserialize(response)));
+  }
 }
