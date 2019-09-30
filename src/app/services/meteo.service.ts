06c3a00e90ab462a8d0d799d6ec03b0942c18959
Lucas <lucas.cheminade@epitech.eu>
2019-09-30 13:27:26 +0300

    FEAT(ServiceMeteo): Meteo service WIP

---
+++ src/app/services/meteo.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeteoService {
+
+private readonly apiKey = '3481483743ad645cbdb84b425581d2f1';
+
+  constructor() { }
+}
