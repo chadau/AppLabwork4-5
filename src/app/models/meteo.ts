46153ce364b5c666060bb1f73fed36560f4ca937
Lucas <lucas.cheminade@epitech.eu>
2019-10-06 15:00:22 +0300

    FEAT(MeteoComponent): Add meteo widget icon
    + Add attribut icon in meteo model and serialize meteo
    + Add img in meteo component

--- src/app/models/meteo.ts
+++ src/app/models/meteo.ts
@@ -1,14 +1,16 @@
 export class Meteo {
-    public city: string;
+    city: string;
     temperature: number;
     humidity: number;
     windSpeed: number;
+    icon: string;
 
     deserialize(input: any): this {
         this.city = input.name;
         this.temperature = input.main.temp;
         this.humidity = input.main.humidity;
         this.windSpeed = input.wind.speed;
+        this.icon = 'http://openweathermap.org/img/wn/' + input.weather[0].icon + '@2x.png';
         return this;
     }
 }
